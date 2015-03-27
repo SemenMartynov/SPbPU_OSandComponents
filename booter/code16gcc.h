6d1fe3ab2990b6df6db3f1269bd90d186a5992e8
Semen Martynov <semen.martynov@gmail.com>
2015-03-27 23:13:14 +0300

    MBR demo

---
+++ booter/code16gcc.h
@@ -0,0 +1,13 @@
+/*
+ * code16gcc.h
+ *
+ *  Created on: Mar 29, 2015
+ *      Author: sam
+ */
+
+#ifndef _CODE16GCC_H_
+#define _CODE16GCC_H_
+
+__asm__(".code16gcc\n");
+
+#endif /* CODE16GCC_H_ */
